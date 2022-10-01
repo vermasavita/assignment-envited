5e78cf328bb3b906406cde039507f0f162c1c792
Savita Verma <svitaverma10@gmail.com>
2022-10-01 20:41:53 +0530

    add link

--- src/pages/createEvent/CreateEvent.jsx
+++ src/pages/createEvent/CreateEvent.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 const CreateEventForm = () => {
   return (
     <div className="landingpage w-full eventheight max-w-sm max-h-sm flex flex-col justify-start p-1 rounded-md lg:w-1/2">
@@ -45,9 +46,9 @@ const CreateEventForm = () => {
               />
             </div>
           </div>
-          <button className="btn md:w-2/3 md:m-auto md:py-2 mt-4 mb-4 text-xl lg:text-lg md:text-base rounded-lg p-2 px-12 md:px-2 text-white font-bold">
+          <Link to="/event" className="btn md:w-2/3 md:m-auto md:py-2 mt-4 mb-4 text-xl lg:text-lg md:text-base rounded-lg p-2 px-12 md:px-2 text-white font-bold">
             Add Your Event
-          </button>
+          </Link>
         </form>
       </section>
     </div>
